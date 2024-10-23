95705d8e6743a37454255893abdbce355fa93e5f
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-10-23 15:48:03 -0400

    Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <nav>Navbar</nav>
+      <Navbar />
       <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
         <Outlet />
       </main>
